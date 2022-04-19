7722a3248bfbebf3d0dce070549a0bc9c08d8283
Ethien Duckett <ethienduckett@outlook.com>
2022-04-19 10:00:03 -0400

    Following tutorial https://www.youtube.com/watch?v=dJ6f2o92tKg called "How to create an Address Finder (MapKit) in SwiftUI Tutorial (2022)" completed section "API Request"

--- AddressFinder/MapModel.swift
+++ AddressFinder/MapModel.swift
@@ -6,3 +6,68 @@
 //
 
 import Foundation
+import MapKit
+
+struct Address: Codable {
+    let data: [Datum]
+}
+
+struct Datum: Codable {
+    let latitude, longitude: Double
+    let name: String?
+}
+
+struct Location: Identifiable {
+    let id = UUID()
+    let name: String
+    let coordinate: CLLocationCoordinate2D
+}
+class MapAPI: ObservableObject {
+    private let BASE_URL = "http://api.positionstack.com/v1/forward"
+    private let API_KEY = "1db14edef6fc08627b92b95bf6252b7f"
+    
+    @Published var region: MKCoordinateRegion
+    @Published var coordinates = []
+    @Published var locations: [Location] = []
+    
+    init () {
+        let lat = 51.50
+        let lon = -0.1275
+        
+        self.region = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: lat, longitude: lon), span: MKCoordinateSpan(latitudeDelta: 5, longitudeDelta: 5) )
+        self.locations.insert(Location(name:"Pin", coordinate: CLLocationCoordinate2D(latitude: lat, longitude: lon)), at: 0)
+    }
+    func getLocation(address: String, delta: Double) {
+        let pAddress = address.replacingOccurrences(of: " ", with: "%20")
+        let url_string = "\(BASE_URL)?access_key=\(API_KEY)&query=\(pAddress)"
+        guard let url = URL(string: url_string) else {
+            print("Invalid URL")
+            return
+        }
+        URLSession.shared.dataTask(with: url) {(data, response, error) in
+            guard let data = data else {
+                print(error!.localizedDescription)
+                return
+            }
+            guard let newCoordinates = try? JSONDecoder().decode(Address.self, from:data) else {return}
+            if newCoordinates.data.isEmpty {
+                print("Could not find the address...")
+                return
+            }
+            DispatchQueue.main.async {
+                let details = newCoordinates.data[0]
+                let lat = details.latitude
+                let lon = details.longitude
+                let name = details.name
+                self.coordinates = [lat, lon]
+                self.region = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: lat, longitude: lon), span: MKCoordinateSpan(latitudeDelta: delta, longitudeDelta: delta))
+                
+                let new_location = Location(name: name ?? "Pin", coordinate: CLLocationCoordinate2D(latitude: lat, longitude: lon))
+                self.locations.removeAll()
+                self.locations.insert(new_location, at: 0)
+                print("Successfully loaded the location!")
+            }
+        }
+        .resume()
+    }
+}
