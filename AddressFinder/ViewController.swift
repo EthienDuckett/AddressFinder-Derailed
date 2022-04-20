c6eccd6307fa707b9a8a90fe922051cd8cd9e431
Ethien Duckett <ethienduckett@outlook.com>
2022-04-20 09:41:17 -0400

    I got the app working after abridging the tutorial.

--- AddressFinder/ViewController.swift
+++ AddressFinder/ViewController.swift
@@ -6,12 +6,28 @@
 //
 
 import UIKit
+import MapKit
 
 class ViewController: UIViewController {
-
+    var mapAPI = MapAPI()
+    
+    @IBOutlet var textField: UITextField!
+    @IBOutlet var map: MKMapView!
+    
+    @IBOutlet var searchField: UITextField!
+    @IBAction func queryWithSearchField(_ sender: Any) {
+        mapAPI.getLocation(address: textField.text!, delta: 0.5)
+        print(mapAPI.locations)
+        map.setCenter(mapAPI.locations[0].coordinate, animated:false)
+        map.setRegion(mapAPI.region, animated:false)
+    }
+    
+    @IBOutlet var Body: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+
     }
 
 
