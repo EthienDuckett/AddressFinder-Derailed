e525420794d9fdd17e5bd2fa7d8c338fb113c6b0
Ethien Duckett <ethienduckett@outlook.com>
2022-04-22 09:58:49 -0400

    I fixed a bug where entering noting into the textField and querying would freeze the app. I added functionality to display multiple locations.

--- AddressFinder/MapModel.swift
+++ AddressFinder/MapModel.swift
@@ -35,16 +35,19 @@ class MapAPI: ObservableObject {
         let lon = -0.1275
         
         self.region = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: lat, longitude: lon), span: MKCoordinateSpan(latitudeDelta: 5, longitudeDelta: 5) )
-        self.locations.insert(Location(name:"Pin", coordinate: CLLocationCoordinate2D(latitude: lat, longitude: lon)), at: 0)
     }
+
     func getLocation(address: String, delta: Double) {
+        locations.removeAll()
         let pAddress = address.replacingOccurrences(of: " ", with: "%20")
-        let url_string = "\(BASE_URL)?access_key=\(API_KEY)&query=\(pAddress)"
+        let url_string = "\(BASE_URL)?access_key=\(API_KEY)&query=\(pAddress)&limit=5&output=json"
         guard let url = URL(string: url_string) else {
             print("Invalid URL")
             return
         }
+        let sem = DispatchSemaphore.init(value:0)
         URLSession.shared.dataTask(with: url) {(data, response, error) in
+            
             guard let data = data else {
                 print(error!.localizedDescription)
                 return
@@ -54,20 +57,23 @@ class MapAPI: ObservableObject {
                 print("Could not find the address...")
                 return
             }
-            DispatchQueue.main.sync {
-                let details = newCoordinates.data[0]
+            print("Inside datatask")
+            for data in newCoordinates.data {
+                let details = data
                 let lat = details.latitude
                 let lon = details.longitude
                 let name = details.name
+                print(name!)
                 self.coordinates = [lat, lon]
                 self.region = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: lat, longitude: lon), span: MKCoordinateSpan(latitudeDelta: delta, longitudeDelta: delta))
                 
-                let new_location = Location(name: name ?? "Pin", coordinate: CLLocationCoordinate2D(latitude: lat, longitude: lon))
-                self.locations.removeAll()
-                self.locations.insert(new_location, at: 0)
-                print("Successfully loaded the location!")
+                let new_location = Location(name: name ?? "Not found", coordinate: CLLocationCoordinate2D(latitude: lat, longitude: lon))
+                self.locations.append(new_location)
             }
+            sem.signal()
         }
         .resume()
+        print("Locations were queried.")
+        sem.wait()
     }
 }
--- AddressFinder/ViewController.swift
+++ AddressFinder/ViewController.swift
@@ -8,19 +8,66 @@
 import UIKit
 import MapKit
 
+class ButtonMod: UIButton {
+    var indice:Int = 0
+}
+
+
 class ViewController: UIViewController {
     var mapAPI = MapAPI()
+    var location_index = 0
     
+    @IBOutlet var locationsView: UIStackView!
     @IBOutlet var textField: UITextField!
     @IBOutlet var map: MKMapView!
+    var location_buttons:[ButtonMod] = []
     
     @IBOutlet var searchField: UITextField!
+    
+    fileprivate func moveMapToLocation() {
+        if mapAPI.locations.count > location_index {
+            print("setting location to \(mapAPI.locations[location_index].name)")
+            map.setCenter(mapAPI.locations[location_index].coordinate, animated:true)
+            map.setRegion(mapAPI.region, animated:true)
+        } else {
+            print("Location not found")
+        }
+    }
+    
     @IBAction func queryWithSearchField(_ sender: Any) {
+        for button in location_buttons {
+            button.removeFromSuperview()
+        }
+        location_buttons.removeAll()
+        mapAPI.locations.removeAll()
+        if textField.text!.isEmpty {
+            return
+        }
         mapAPI.getLocation(address: textField.text!, delta: 0.5)
         print(mapAPI.locations)
-        map.setCenter(mapAPI.locations[0].coordinate, animated:false)
-        map.setRegion(mapAPI.region, animated:false)
+        moveMapToLocation()
+        for i in 0..<mapAPI.locations.count {
+            print("iteration")
+            location_buttons.append(ButtonMod(type: ButtonMod.ButtonType.system))
+            location_buttons[i].frame = CGRect(x:0, y:20*i, width:150, height:20)
+//            button.backgroundColor = UIColor.blue
+            location_buttons[i].setTitle(mapAPI.locations[i].name, for: .normal)
+            location_buttons[i].setTitleColor(UIColor.systemBlue, for: .normal)
+            
+            location_buttons[i].indice = i
+            location_buttons[i].addTarget(self,
+                                          action: #selector(changeButtonLocation),
+                                          for:.touchUpInside)
+            locationsView.addSubview(location_buttons[i])
+        }
     }
+    @IBAction func changeButtonLocation(sender:ButtonMod) {
+        print("set location_index to \(sender.indice)")
+        location_index = sender.indice
+        moveMapToLocation()
+    }
+    
+    
     
     @IBOutlet var Body: UIView!
     
